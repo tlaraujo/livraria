0b1423a5da1a8460a35676c774ba5dfd74b0433a
Thiago L.. Araújo <tlaraujo@accesys.local>
2017-12-07 15:46:59 -0200

    first commit

---
+++ src/CalculadoraDeEstoque.java
@@ -0,0 +1,12 @@
+public class CalculadoraDeEstoque {
+    public static void main(String[] args){
+
+        double livroJava8 = 59.90;
+        double livroTDD   = 59.90;
+
+        double soma = livroJava8 + livroTDD;
+
+        System.out.println("O total em estoque é "+soma);
+
+    }
+}
